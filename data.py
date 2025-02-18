7f56c87336f7a86d8522d9c1d0c81dc32b57559c
Vladislav Spirin <spirin200@yandex.ru>
2025-02-18 16:14:00 +0300

    Update data.py

--- data.py
+++ data.py
@@ -29,7 +29,7 @@ def get_cheapest_flats_by_rooms(room_number):
     chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("disable-infobars")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")  # Подмена User-Agent
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36") 
     chrome_options.add_argument("--disable-dev-shm-usage")  
 
     service = Service(executable_path='')  
